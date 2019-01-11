ec44b0ebc5dff42f1117500755c4a4e57a2a7cca
Raul Rodriguez-Esteban <raul.rodriguez-esteban@roche.com>
2019-01-11 14:26:07 +0100

    change in variable names

--- read_ambiguous_gene_annotation_synonyms.py
+++ read_ambiguous_gene_annotation_synonyms.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# this script reads NCBI Gene gene names and synonyms 
+# this script reads NCBI Gene gene names and synonyms
 # and identifies those gene names that are ambiguous because they share at least a synonym
 
 # NCBI Gene file
@@ -10,44 +10,45 @@ output_file = "ambiguous_names_and_annotations.txt"
 # statistics on number of synonyms per gene
 results_file = "synonyms_per_gene.txt"
 
+
 import re 
 import gzip
 
 f = gzip.open(input_file, 'rt')
 
-# read gene names, IDs and synonyms from NCBI Gene data
-gene_strings = []
-synonym_to_gene_id = {}
-total_genes = 0
+# read MeSH IDs, names and synonyms
+annotation_strings = []
+synonym_to_annotation_id = {}
+total_annotations = 0
 total_synonyms = 0
 for line in f:
     line = line[:-1]
     data = line.split("\t")
     # NCBI Gene data of interest
     species = data[0]
-    gene_id = data[1]
-    gene_name = data[2]
-    gene_synonyms = data[4].split("|")
-    gene_synonyms.append(gene_name)
+    annotation_id = data[1]
+    annotation_name = data[2]
+    annotation_synonyms = data[4].split("|")
+    annotation_synonyms.append(annotation_name)
     # only information related to humans is of interest (species number 9606)
     if species == "9606":
-        total_genes += 1
-        for gene_synonym in gene_synonyms:
-            # a gene synonym needs to include at least a letter
-            if re.search("[a-zA-Z]", gene_synonym):
-                # gene synonyms are considered case-insensitively so they are changed to lower case
-                gene_synonym = gene_synonym.lower()
-                # check if gene synonym has already been seen
-                if gene_synonym in synonym_to_gene_id.keys():
-                    # if so, then add the gene ID to the list of gene IDs associated to that synonym
-                    if gene_id not in synonym_to_gene_id[gene_synonym]:
-                        synonym_to_gene_id[gene_synonym].append(gene_id)
+        total_annotations += 1
+        for annotation_synonym in annotation_synonyms:
+            # an annotation synonym needs to include at least a letter
+            if re.search("[a-zA-Z]", annotation_synonym):
+                # annotation synonyms are considered case-insensitively so they are changed to lower case
+                annotation_synonym = annotation_synonym.lower()
+                # check if annotation synonym has already been seen
+                if annotation_synonym in synonym_to_annotation_id.keys():
+                    # if so, then add the annotation ID to the list of annotation IDs associated to that synonym
+                    if annotation_id not in synonym_to_annotation_id[annotation_synonym]:
+                        synonym_to_annotation_id[annotation_synonym].append(annotation_id)
                 else:
                     # if not, create a new entry for the synonym
-                    synonym_to_gene_id[gene_synonym] = [gene_id]
+                    synonym_to_annotation_id[annotation_synonym] = [annotation_id]
                     total_synonyms += 1
 
-print("Total (human) genes: " + str(total_genes))
+print("Total annotation: " + str(total_annotations))
 print("Total synonyms: " + str(total_synonyms))
 
 # Write an output file with the data read in the previous section
@@ -56,34 +57,34 @@ f_out = open(output_file, "w")
 count_ambiguous = 0
 count_non_ambiguous = 0
 
-ambiguous_gene_list = {}
-gene_ids_per_ambiguous_synonym = {}
-for gene_synonym in synonym_to_gene_id.keys():
-    number_of_ids = len(synonym_to_gene_id[gene_synonym])
-    # keep statistic on the number of gene IDs that are associated to each synonym
-    if number_of_ids not in gene_ids_per_ambiguous_synonym:
-        gene_ids_per_ambiguous_synonym[number_of_ids] = 0
-    gene_ids_per_ambiguous_synonym[number_of_ids] += 1
-    # only gene synonyms associated to more than one gene ID are considered in the output
+ambiguous_annotation_list = {}
+annotation_ids_per_ambiguous_synonym = {}
+for annotation_synonym in synonym_to_annotation_id.keys():
+    number_of_ids = len(synonym_to_annotation_id[annotation_synonym])
+    # keep statistic on the number of annotation IDs that are associated to each synonym
+    if number_of_ids not in annotation_ids_per_ambiguous_synonym:
+        annotation_ids_per_ambiguous_synonym[number_of_ids] = 0
+    annotation_ids_per_ambiguous_synonym[number_of_ids] += 1
+    # only annotation synonyms associated to more than one annotation ID are considered in the output
     if number_of_ids > 1:
-        f_out.write(gene_synonym + "\t" + "|".join(synonym_to_gene_id[gene_synonym]) + "\n")
+        f_out.write(annotation_synonym + "\t" + "|".join(synonym_to_annotation_id[annotation_synonym]) + "\n")
         count_ambiguous+=1
-        # add the gene IDs to a list of ambiguous gene IDs
-        for gene_id in synonym_to_gene_id[gene_synonym]:
-            if gene_id not in ambiguous_gene_list.keys():
-                ambiguous_gene_list[gene_id] = 1
+        # add the annotation IDs to a list of ambiguous annotation IDs
+        for annotation_id in synonym_to_annotation_id[annotation_synonym]:
+            if annotation_id not in ambiguous_annotation_list.keys():
+                ambiguous_annotation_list[annotation_id] = 1
     else:
         count_non_ambiguous+=1
 
-# Write an output table with the statistics on number of gene IDs per synonym
+# Write an output table with the statistics on number of annotation IDs per synonym
 print("Writing synonym counts to " + results_file + "...")
 f_out = open(results_file, "w")
-f_out.write("Synonym count\tNumber of genes\n")
-for i in range(1,max(gene_ids_per_ambiguous_synonym.keys())+1):
-    if i in gene_ids_per_ambiguous_synonym.keys():
-        count = gene_ids_per_ambiguous_synonym[i]
+f_out.write("Synonym count\tNumber of annotations\n")
+for i in range(1,max(annotation_ids_per_ambiguous_synonym.keys())+1):
+    if i in annotation_ids_per_ambiguous_synonym.keys():
+        count = annotation_ids_per_ambiguous_synonym[i]
     else:
         count = 0
     f_out.write(str(i) + "\t" + str(count) + "\n")
 
-print("Genes with ambiguous synonyms: " + str(len(ambiguous_gene_list.keys())))
+print("Annotations with ambiguous synonyms: " + str(len(ambiguous_annotation_list.keys())))
